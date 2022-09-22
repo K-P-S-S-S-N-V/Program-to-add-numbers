1710493f234a9628f2c94ec347084ba785028aa6
POORNA VENKATESH KADALI <110458170+K-P-S-S-S-N-V@users.noreply.github.com>
2022-09-22 18:29:28 +0530

    Add files via upload

---
+++ adding two numbers (model 3).py
@@ -0,0 +1,5 @@
+X = input("Enter number: ")      #Here "X" and "Y" are input values & Variables 
+Y = input("Enter another number: ")
+
+sum = int(X) + int(Y)            #int = integer
+print("The sum is: ", sum)
